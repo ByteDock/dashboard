fbf4bf0741b6c111d02c2c2c2c0f9c80a86dd1ab
Mads Lundgren <contact@crabzzai.me>
2024-01-08 23:58:43 +0100

    Network and project pages added

---
+++ pages/infrastructure/networks.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="pl-4 pr-4 sm:ml-64 min-h-screen items-stretch">
+    <BannerComponent
+      title="Network Nook"
+      text="A cozy corner for networks to gossip about packets, share memes, and connect with each other. Don't interrupt their digital chit-chat!"
+      svg="UndrawContainerShip"
+      :btn="{
+        text: 'Create a new network'
+      }"
+    />
+  </div>
+</template>
+
+<script>
+definePageMeta({
+  name: 'InfrastructureNetworks'
+})
+</script>
---
+++ pages/infrastructure/projects.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="pl-4 pr-4 sm:ml-64 min-h-screen items-stretch">
+    <BannerComponent
+      title="Project Playroom"
+      text="A playroom for projects to tinker, doodle, and embark on creative adventures. Bring your project toys for some coding fun!"
+      svg="UndrawChildren"
+      :btn="{
+        text: 'Create a new project'
+      }"
+    />
+  </div>
+</template>
+
+<script>
+definePageMeta({
+  name: 'InfrastructureProjects'
+})
+</script>
